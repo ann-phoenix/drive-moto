6540b1a55a04e5949ac2b2266bf32ab3a04c44f2
ann-phoenix <ann.phoenix.it@gmail.com>
2020-09-01 19:56:43 +0300

    added products-popular, products sections and banner

--- src/js/main.js
+++ src/js/main.js
@@ -5,17 +5,31 @@ $(function () {
 	 dots: true,
 	 prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/icons/arrow-left.svg" alt=""></button>',
 	 nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/icons/arrow-right.svg" alt=""></button>',
+ });
+
+ /*Product slider*/
+ $('.product-slider').slick({
+	 slidesToShow: 4,
+	 slidesToScroll: 1,
+	 prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/icons/arrow-black-left.svg" alt=""></button>',
+	 nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/icons/arrow-black-right.svg" alt=""></button>',
+
+ });
+
+ /*Tabs*/
+ $('.tab').on('click', function(e){
+	 e.preventDefault();
+
+	 $($(this).siblings()).removeClass('tab--active');
+	 $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
+	 $(this).addClass('tab--active');
+	 $($(this).attr('href')).addClass('tabs-content--active');
+ });
+
+/*Favorite active heart*/
+ $('.product-item__favorite').on('click', function(){
+	 $(this).toggleClass('product-item__favorite--active')
  })
 
- /*Search tabs*/
-
- $('.search__tabs-item').on('click', function(event){
-	 event.preventDefault();
-
-	 $('.search__tabs-item').removeClass('search__tabs-item--active');
-	 $('.search__content-item').removeClass('search__content-item--active');
-
-	 $(this).addClass('search__tabs-item--active');
-	 $($(this).attr('href')).addClass('search__content-item--active')
- });
 });
